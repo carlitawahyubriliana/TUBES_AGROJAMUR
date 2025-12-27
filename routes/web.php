2a83ebb7b8f1d992f993bc99d6c79829ae26ec6c
Carlita Wahyu Briliana <carlitabriliana555@gmail.com>
2025-12-27 13:28:23 +0700

    homepage

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,12 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('index');
 });
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/homepage', function () {
+    return view('homepage');
+});
+
